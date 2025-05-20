f58afce6a00058223f7b7db74e7416f606cbfc8e
Raquel0409 <santosrauel0409r@gmail.com>
2025-05-20 16:40:33 -0300

    variaveis, variaveis, alert, console.log

---
+++ logicacomjs.js
@@ -0,0 +1,25 @@
+/*let bruna =6.5; 
+let carla = 8; 
+let soma = bruna + carla 
+console.log("A soma é: " + soma)
+console.log("--------------------------")
+let dividaemjaneiro = 1000;
+let pagoaemnovembro = 700;
+let oquantodevo= dividaemjaneiro - pagoaemnovembro
+console.log("A subtração da divida é: " + oquantodevo + "R$")
+console.log("--------------------------")
+let salárioiniciodoano=1500;
+let saláriofimdoano=6;
+let totalanual= salárioiniciodoano * saláriofimdoano
+console.log("A multiplicação do ano é: " + totalanual+ "R$")
+console.log("--------------------------")
+let irmaos= 10;
+let herança= 2;
+let divisaodaherança= irmaos/herança
+console.log("A divisão da herança é: " + divisaodaherança+"R$")
+console.log("--------------------------")*/
+let mensal=6000;
+let trabalhados= 12;
+let meses=12
+let decimo= mensal/trabalhados*meses
+console.log("Raquel o seu 13° é; "+decimo+ "R$ "+ "Parabéns!")
